30cf06a92939447f41ba687939e83c176e9065cd
GOZEBRAHEAD <54969894+GOZEBRAHEAD@users.noreply.github.com>
2021-04-25 15:57:38 -0300

    Upload Guia 5 - TPO 1

---
+++ Guia 5/Ejercicios/src/Logica/Main.java
@@ -0,0 +1,21 @@
+package Logica;
+
+import GUI.Principal;
+
+/**
+ *
+ * @author Lucho
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Creo la instancia
+        Principal ventanaPrincipal = new Principal();
+        
+        // La hago visible y la pongo en el centro de la pantalla
+        ventanaPrincipal.setVisible(true);
+        ventanaPrincipal.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ Guia 5/README.md
@@ -0,0 +1,8 @@
+<h1>Guia 5 - Ejercicios</h1>
+
+<hr>
+
+![image](https://user-images.githubusercontent.com/54969894/116005747-b56bdd80-a5de-11eb-9a3c-14afbef4cb6c.png)
+
+![image](https://user-images.githubusercontent.com/54969894/116005756-c1579f80-a5de-11eb-98d3-50b729edd104.png)
+
