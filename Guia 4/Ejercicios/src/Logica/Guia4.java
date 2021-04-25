6f77f83c463f5c23649bc53ece855fd8b6a8cca5
GOZEBRAHEAD <54969894+GOZEBRAHEAD@users.noreply.github.com>
2021-04-25 15:35:57 -0300

    Uploaded Guia 4

---
+++ Guia 4/Ejercicios/src/Logica/Guia4.java
@@ -0,0 +1,19 @@
+package Logica;
+
+import GUI.Principal;
+
+/**
+ *
+ * @author Lucho
+ */
+public class Guia4 {
+
+    public static void main(String[] args) {
+        
+        Principal ventanaPrincipal = new Principal();
+        
+        ventanaPrincipal.setVisible(true);
+        ventanaPrincipal.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ Guia 4/README.md
@@ -0,0 +1,7 @@
+<h1>Guia 4 - Ejercicios</h1>
+
+<hr>
+
+![image](https://user-images.githubusercontent.com/54969894/116005029-ab94ab00-a5db-11eb-9b26-639a819da3b1.png)
+
+![image](https://user-images.githubusercontent.com/54969894/116005057-bb13f400-a5db-11eb-8f3a-03f61ae8f934.png)
